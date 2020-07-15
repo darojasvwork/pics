57cc1acff5588067550f7a41c02629dd5fa695c0
darojasvwork <darojasv@gmail.com>
2020-07-15 03:08:28 +0200

    Updates

---
+++ precache-manifest.9032b79d85ffcd518e145460dee4bfee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880298a35a1e4c31d2929680882d74da",
+    "url": "/pics/index.html"
+  },
+  {
+    "revision": "ffd80e01d43870bb41b7",
+    "url": "/pics/static/css/main.5678862c.chunk.css"
+  },
+  {
+    "revision": "71752e1f254d7fdddf9d",
+    "url": "/pics/static/js/2.3f040f9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pics/static/js/2.3f040f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffd80e01d43870bb41b7",
+    "url": "/pics/static/js/main.78d72e7e.chunk.js"
+  },
+  {
+    "revision": "5824943620b7e0a4473b",
+    "url": "/pics/static/js/runtime-main.c0044d51.js"
+  }
+]);
